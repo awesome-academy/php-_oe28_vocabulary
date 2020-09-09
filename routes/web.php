2e5c72e35a172980ee43f213467b6b23e34bea6b
kiennt175 <kiennt175@gmail.com>
2020-09-09 16:25:44 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
